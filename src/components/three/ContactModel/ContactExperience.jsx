decdd4bfa86de55e0c8d14a90717db36889560f7
German-Zero <germannaz_@hotmail.com>
2025-11-22 19:26:31 -0300

    Contact Section

---
+++ src/components/three/ContactModel/ContactExperience.jsx
@@ -0,0 +1,28 @@
+import { Canvas } from '@react-three/fiber'
+import { Coffe } from './Coffe'
+import { OrbitControls } from '@react-three/drei'
+
+const ContactExperience = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 15], fov: 35 }}>
+
+        <OrbitControls 
+            enablePan={false}
+            enableZoom={false}
+            maxDistance={10}
+            minDistance={5}
+            minPolarAngle={Math.PI / 5}
+            maxPolarAngle={Math.PI / 2}
+        />
+
+        <group
+            position={[0, -0.5, 0]}
+            rotation={[0.1, -Math.PI / 6, 0]}
+        >
+            <Coffe />
+        </group>
+    </Canvas>
+  )
+}
+
+export default ContactExperience
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import ContactExperience from "../components/three/ContactModel/ContactExperience"
+import IconGithub from '../assets/icons/IconGithub.png'
+import IconLinkedin from '../assets/icons/IconLinkedin.png'
+import IconInstagram from '../assets/icons/IconInstagram.png'
 
 const Contact = () => {
   return (
     <div className='w-screen h-screen bg-black'>
         <div className='w-full h-full flex justify-center items-center'>
-            <div className='max-w-[1100px] max-h-[700px] w-full h-full'>
-                <div className='relative w-full h-full flex items-center justify-center'>
-                    <div className='absolute w-full z-1 flex flex-col gap-6'>
-                        <h3 className='w-[525px] h-[100px] bg-[#37009C] rounded-xl'></h3>
-                        <p className='w-[210px] h-[25px] bg-[#37009C] rounded-lg'></p>
+            <div className='max-w-[1300px] max-h-[700px] w-full h-full'>
+                <div className='relative w-full h-full flex items-center justify-between'>
+                    <div className='absolute w-auto z-1 flex flex-col gap-6'>
+                        <h3 className='text-white font-sans font-bold text-4xl tracking-wider'>
+                            Te Dejo Como <span className="animated-gradient">Encontrarme!</span>
+                        </h3>
+                        <div className='flex gap-6'>
+                            <a href="https://github.com/German-Zero" target="_blank">
+                                <img src={IconGithub} alt="icongithub" className="w-12 h-12" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/germannaz/" target="_blank">
+                                <img src={IconLinkedin} alt="iconlinkedin" className="w-12 h-12" />
+                            </a>
+                            <a href="https://www.instagram.com/gszero_/" target="_blank">
+                                <img src={IconInstagram} alt="iconinstagram" className="w-12 h-12" />
+                            </a>
+                        </div>
                     </div>
-                    <div className='w-full justify-end flex'>
-                        <div className='w-[700px] h-[682px] bg-[#120033] rounded-full'/>
-                    </div>
+                    <figure className='absolute w-full max-w-[700px] h-full right-14'>
+                        <ContactExperience />
+                    </figure>
                 </div>
             </div>
         </div>
