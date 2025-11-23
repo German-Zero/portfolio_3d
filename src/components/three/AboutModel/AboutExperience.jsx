34b6321d54c3990f92820ad9176fbad461e3d335
German-Zero <germannaz_@hotmail.com>
2025-11-22 21:30:53 -0300

    Details Model3D

---
+++ src/components/three/AboutModel/AboutExperience.jsx
@@ -0,0 +1,28 @@
+import { Canvas } from '@react-three/fiber'
+import { Mugi } from './Mugi'
+import { OrbitControls } from '@react-three/drei'
+
+const AboutExperience = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 15], fov: 55 }}>
+
+        <OrbitControls 
+            enablePan={false}
+            enableZoom={false}
+            maxDistance={10}
+            minDistance={5}
+            minPolarAngle={Math.PI / 5}
+            maxPolarAngle={Math.PI / 2}
+        />
+        
+        <group
+            position={[0, -3, 0]}
+            rotation={[0.1, -Math.PI / 2, -0.1]}
+        >
+            <Mugi />
+        </group>
+    </Canvas>
+  )
+}
+
+export default AboutExperience
--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import AboutExperience from '../components/three/AboutModel/AboutExperience'
 
 const AboutMe = () => {
   return (
-    <div className='w-screen h-screen bg-black flex justify-center items-center m-auto'>
-        <div className='flex justify-between w-3/4'>
-            <div className='w-auto h-auto justify-center flex flex-col gap-14 items-center'>
-                <h2 className='w-full max-w-[1200px] h-auto text-white text-start font-sans text-5xl'>Permiteme Presentarme.</h2>
-                <p className='w-full max-w-[700px] text-white font-heading text-2xl text-start font-medium tracking-widest leading-9'>
-                  Soy un desarrollador en formacion con foco en <span className='text-[#40ff00]'>backend y frontend</span>,
-                  en busca constante de desafios que ponga a prueba mis habilidades y conocimientos.
-                  <br /><br />
-                  Estoy predispuesto a tirarme en el barro si fuese necesario con tal de superarme
-                  a mi mismo y mis propios limites.
-                </p>
-            </div>
-            <div className='w-[450px] h-[450px] bg-[#120033] rounded-full'></div>
+    <div className='w-screen h-screen bg-black'>
+      <div className='w-full h-full flex justify-center items-center'>
+        <div className='w-3/4 h-full max-h-[700px] flex flex-col justify-between items-center'>
+          <div className='relative flex items-center justify-between h-full w-full'>
+              <div className='absolute w-auto max-w-[700px] z-1 flex flex-col gap-14'>
+                  <h2 className='w-full h-auto text-white text-start font-sans text-5xl'>Permiteme Presentarme.</h2>
+                  <p className='w-full text-white font-heading text-2xl text-start font-medium tracking-widest leading-9'>
+                    Soy un desarrollador en formacion con foco en <span className='text-[#40ff00]'>backend y frontend</span>,
+                    en busca constante de desafios que ponga a prueba mis habilidades y conocimientos.
+                    <br /><br />
+                    Estoy predispuesto a tirarme en el barro si fuese necesario con tal de superarme
+                    a mi mismo y mis propios limites.
+                  </p>
+              </div>
+              <figure className='absolute flex h-full w-full max-w-[700px] max-h-[700px] right-14'>
+                <AboutExperience />
+              </figure>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
