54c64dfe57d4973900b470f6863699940b1febc0
German-Zero <germannaz_@hotmail.com>
2025-11-28 17:55:03 -0300

    Analytics Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/next'
+
 import AboutMe from './sections/AboutMe'
 import Contact from './sections/Contact'
 import Hero from './sections/Hero'
@@ -12,6 +14,7 @@ function App() {
       <SkillTech />
       <Projects />
       <Contact />
+      <Analytics />
     </>
   )
 }
